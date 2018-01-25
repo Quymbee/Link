2713561ec4a011c796349dc5a1696c5566c0df56
Quymbee <quymbee@mattchen.com>
2018-01-25 15:28:11 -0500

    gimbal

--- hello-gimbal-world/app/src/main/java/com/gimbal/hello_gimbal_android/MainActivity.java
+++ hello-gimbal-world/app/src/main/java/com/gimbal/hello_gimbal_android/MainActivity.java
@@ -1,5 +1,7 @@
 package com.gimbal.hello_gimbal_android;
 
+import java.util.Locale;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,48 +9,56 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.app.Activity;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends Activity {
     public GimbalEventReceiver gimbalEventReceiver;
     public GimbalEventListAdapter adapter;
 
+   // public TextToSpeech tts;
+
     public TextView beaconTextView;
     public TextView addressTextView;
     public String address;
     public String temp;
+ //   public String temp1 = " ";
+   // public String fullTemp;
+    //public String beaconTxt;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startService(new Intent(this, AppService.class));
-
         adapter = new GimbalEventListAdapter(this);
-        /*
-        Intent intent = new Intent(this, AppService.class);
-        */
 
-        /*
-        ListView listView = (ListView) findViewById(R.id.listview);
-        listView.setAdapter(adapter);
-        */
 
         beaconTextView = findViewById(R.id.beaconText);
         addressTextView = findViewById(R.id.addressText);
 
-        address = "10th Ave and W 30th St";
         temp = AppService.placee;
+        address = "10th Ave and W 30th St is the location of the nearest Link";
 
-
-
-        beaconTextView.setText(temp);
         addressTextView.setText(address);
-        //txtview.setText(AppService.placee);
+        beaconTextView.setText(temp);
 
+        //  speakOut();
 
 
 
     }
+/*
+    public void speakOut(){
+       // CharSequence txt = beaconTextView.getText();
+        CharSequence txt = "I hope this works!";
+        tts.speak(txt, TextToSpeech.QUEUE_ADD, null, "id1");
+    }
+*/
 
 
     @Override
@@ -87,7 +97,15 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onDestroy() {
+        /*
+        if (tts != null){
+            tts.stop();
+            tts.shutdown();
+        }
+        */
+
         super.onDestroy();
+
     }
 
     class GimbalEventReceiver extends BroadcastReceiver {
@@ -95,8 +113,34 @@ public class MainActivity extends Activity {
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction() != null) {
                 if (intent.getAction().compareTo(GimbalDAO.GIMBAL_NEW_EVENT_ACTION) == 0) {
+
+                   // temp1 = temp;
+
+                 //   fullTemp = "Temp: " + temp + " and Temp1: " + temp1;
+
+
+                    /*
+                    beaconTxt = beaconTextView.getText().toString();
+
+                    if(beaconTxt.equals("There are no beacons found!"))
+                    {
+                        address = " ";
+                    } else {
+                        address = "10th Ave and W 30th St is the location of the nearest Link";
+                    }
+                    */
+
+                    /*
+                    if (temp1.equalsIgnoreCase(temp)) {
+                        Log.v("equals", fullTemp);
+                    } else{
+                        Log.v("not equals", fullTemp);
+                    }
+*/
                     adapter.setEvents(GimbalDAO.getEvents(getApplicationContext()));
+
                 }
+
             }
         }
     }
